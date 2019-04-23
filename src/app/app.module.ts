1716070cec1962bf4bb5ca732628991e365b997f
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-04-24 00:13:30 +0100

    app module upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { ThemeModule } from './theme/theme.module';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
