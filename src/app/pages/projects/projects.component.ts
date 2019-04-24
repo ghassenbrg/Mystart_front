24c5d72c40a4dcb8017041886c619c12f4cef847
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-04-24 22:11:42 +0100

    add projects list

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -14,6 +14,33 @@ export class ProjectsComponent implements OnInit {
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."
   }
 
+  projects = [
+    {
+      title: "Complete Python Bootcamp",
+      description: "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.",
+      author: "Ghassen Bargougui",
+      coverImg: "../../../assets/images/courses/courses-gird-v1-1.png"
+    },
+    {
+      title: "Complete Python Bootcamp",
+      description: "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.",
+      author: "Elwaer Youcef",
+      coverImg: "../../../assets/images/courses/courses-gird-v1-2.png"
+    },
+    {
+      title: "Complete Python Bootcamp",
+      description: "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.",
+      author: "Timoumi Khlalil",
+      coverImg: "../../../assets/images/courses/courses-gird-v1-3.png"
+    },
+    {
+      title: "Complete Python Bootcamp",
+      description: "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.",
+      author: "Hasni Iheb",
+      coverImg: "../../../assets/images/courses/courses-gird-v1-4.png"
+    }
+  ];
+
   constructor( private title: Title) {
     this.title.setTitle("Mystart | Projects")
    }
