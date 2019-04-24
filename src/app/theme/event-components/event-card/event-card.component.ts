7bf850d6afd3bf7987694545a05a1bb4de20c986
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-04-24 20:28:25 +0100

    event single

--- src/app/theme/event-components/event-card/event-card.component.ts
+++ src/app/theme/event-components/event-card/event-card.component.ts
@@ -7,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventCardComponent implements OnInit {
 
+  events =[
+    {
+      title: "Tales from the White House and beyond",
+      description: "Lorem Ipsn gravida nibh velalito auctor alipuet. Aenean sollicitudin, lorem quis bibendum auci elit conse ipsutis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus.",
+      day: "Sunday",
+      dateD: 15,
+      dateM: "December",
+      dateT: "9.00 am",
+      location: "Monastir, TN",
+      organizer: "Google",
+      organizerDesc: "Google LLC is an American multinational technology company",
+      coverImg: "../../../../assets/images/events/event-grid-1.png",
+      organizerPic: "../../../../assets/images/events/author-teacher.png"
+    },
+    {
+      title: "Lorem Ipsn gravida nibh velalito auctor alipuet",
+      description: "Lorem Ipsn gravida nibh velalito auctor alipuet. Aenean sollicitudin, lorem quis bibendum auci elit conse ipsutis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus.",
+      day: "Monday",
+      dateD: 28,
+      dateM: "April",
+      dateT: "14.30 am",
+      location: "Sousse, TN",
+      organizer: "ISIMM",
+      organizerDesc: "Aenean sollicitudin, lorem quis bibendum auci elit",
+      coverImg: "../../../../assets/images/events/event-grid-2.png",
+      organizerPic: "../../../../assets/images/events/author-teacher.png"
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
