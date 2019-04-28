e0736c5c020da8dc2510daa07a49a5e5f3be7127
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-04-28 21:08:46 +0100

    event-single consume RestApi

--- src/app/pages/event-single/event-single.component.ts
+++ src/app/pages/event-single/event-single.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { RestApiService } from 'src/app/core/rest-api.service';
 
 @Component({
   selector: 'app-event-single',
@@ -12,20 +13,25 @@ export class EventSingleComponent implements OnInit {
   id: any;
 
   params = {
-    title: "My Title",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+    title: "",
+    description: "",
     path: "Events"
   }
 
+  event = {};
 
-  constructor( private title: Title, private route: ActivatedRoute) {
-    this.title.setTitle("Mystart | My Title");
+  constructor( private title: Title, private route: ActivatedRoute, public restApi: RestApiService) {
    }
 
   ngOnInit() {
 
     this.route.paramMap.subscribe(params => {
       this.id= params.get('id');
+      return this.restApi.get('event/'+this.id).subscribe((data: {}) => {
+        this.event = data;
+        this.params.title = data['title'];
+        this.title.setTitle("Mystart | "+data['title']);
+      });
     });
 
   }
