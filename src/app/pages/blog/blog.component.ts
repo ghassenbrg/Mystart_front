d5bcf01343e9b5c27b91e061e41ed6bfce83c0c6
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-04-30 15:10:20 +0100

    blog page with articles list

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { RestApiService } from 'src/app/core/rest-api.service';
+import { TimeService } from 'src/app/core/time.service';
+import { post } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-blog',
@@ -7,12 +11,26 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+
+  posts: any;
+  dates = [];
   
-  constructor( private title: Title) {
+  constructor(private title: Title, private route: ActivatedRoute, public restApi: RestApiService, private time: TimeService) {
     this.title.setTitle("Mystart | Blog")
    }
 
   ngOnInit() {
+
+    return this.restApi.get('articles').subscribe((data: {}) => {
+      this.posts = data;
+      let i = 0;
+      for (let post of this.posts){
+        post.content = post.content.substring(0,170).replace(/<[^>]*>/g, '')+"...";
+        this.dates[i] = new Date(post.creationDate);
+        i++;
+      }
+    });
+
   }
 
 }
