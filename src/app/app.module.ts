930585bcf3fdc90f54fc54508f5235c0d8c15c3b
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-05-09 02:54:47 +0100

    Revert "add ng-zorro"
    This reverts commit 140b18c24ceb3d0654b24a3c55c0ccdcda2cc6fb.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,6 @@ import { RestApiService } from './core/rest-api.service';
 import { FacebookModule } from 'ngx-facebook';
 import { TestComponent } from './test/test.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-
-registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -31,12 +24,9 @@ registerLocaleData(en);
     ThemeModule,
     HttpClientModule,
     FacebookModule.forRoot(),
-    NgbModule,
-    NgZorroAntdModule,
-    FormsModule,
-    BrowserAnimationsModule
+    NgbModule
   ],
-  providers: [RestApiService, { provide: NZ_I18N, useValue: en_US }],
+  providers: [RestApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
