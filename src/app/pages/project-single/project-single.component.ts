29d9e4d6df3f8d546fea216823ab16954bbcb20d
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-05-09 02:35:20 +0100

    hide if not authorized

--- src/app/pages/project-single/project-single.component.ts
+++ src/app/pages/project-single/project-single.component.ts
@@ -19,28 +19,25 @@ export class ProjectSingleComponent implements OnInit {
   }
 
   project: any;
-
+  authorized: boolean;
   constructor( private title: Title, private router: Router, private route: ActivatedRoute, public restApi: RestApiService) {
    }
 
   ngOnInit() {
-
+    this.authorized = true;
     let auth = JSON.parse(localStorage.getItem('auth'));
 
     this.route.paramMap.subscribe(params => {
       this.id= params.get('id');
       return this.restApi.get('project/'+this.id).subscribe((data: {}) => {
-        console.log('test: '+data[0].private);
         if (!data[0].published) {
           this.router.navigate(['/404']);
           return;
         }
         if (data[0].private)
           if ((auth.user_id != data[0].author) && (!data[0].authorized.includes(auth.user_id))){
-            this.router.navigate(['/events']);
-            return;
+            this.authorized = false;
           }
-
         this.project = data[0];
         this.params.title = data[0]['title'];
         this.title.setTitle("Mystart | "+data[0]['title']);
