822604945d17e525fcd9d9483198b2a8b373fd83
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-05-10 19:16:58 +0100

    add filter & sort by [integration only]

--- src/app/theme/event-components/event-card/event-card.component.ts
+++ src/app/theme/event-components/event-card/event-card.component.ts
@@ -12,9 +12,33 @@ export class EventCardComponent implements OnInit {
   events: any;
   startTime = [];
 
+  pagination = {
+    cuurentPage: 1,
+    pageSize: 4
+  }
+  loading = false;
+
+  categories;
+  filter = {};
+  sortBy = {};
+
+  country: String;
+  location = {
+    TN: ['Ariana', 'Béja', 'Ben Arous', 'Bizerte', 'Gabès', 'Gafsa', 'Jendouba', 'Kairouan', 'Kasserine', 'Kebili',
+     'Kef', 'Mahdia', 'Manouba', 'Medenine', 'Monastir', 'Nabeul', 'Sfax', 'Sidi Bouzid', 'Siliana', 'Sousse', 'Tataouine',
+     'Tozeur', 'Tunis', 'Zaghouan']
+  }
+
   constructor(public restApi: RestApiService, private time: TimeService) { }
 
   ngOnInit() {
+    
+    this.filter['keywords'] = '';
+    this.filter['price'] = 'All';
+    this.filter['country'] = null;
+    this.filter['time'] ="Any time";
+    this.sortBy['direction'] = '1'; // 1 for Asc & -1 for Desc
+
     this.restApi.get('events').subscribe((data: {}) => {
       this.events = data;
       // var i = 0;
