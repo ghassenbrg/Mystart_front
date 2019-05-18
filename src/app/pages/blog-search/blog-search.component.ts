7c6f3ed7e2abbb218d541652d503b14dd359b214
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-05-18 19:18:44 +0100

    add filter html components

--- src/app/pages/blog-search/blog-search.component.ts
+++ src/app/pages/blog-search/blog-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from 'src/app/core/rest-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-blog-search',
@@ -22,12 +25,15 @@ export class BlogSearchComponent implements OnInit {
 
   loading = false;
 
-  constructor() { }
+  constructor(private title: Title, private route: ActivatedRoute, public restApi: RestApiService) {
+    this.title.setTitle('Mystart | Blog search');
+   }
 
   ngOnInit() {
     
     this.route.paramMap.subscribe(params => {
-      this.id= params.get('id');
+      this.category= params.get('cat');
+      this.keywords= params.get('keyword');
       
     });
 
