7406cb9873a149fbf403758f1fe7c2084ea3840a
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-05-20 07:28:34 +0100

    display list of crouses from api

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { RestApiService } from 'src/app/core/rest-api.service';
+import { TimeService } from 'src/app/core/time.service';
 
 @Component({
   selector: 'app-courses',
@@ -14,12 +17,36 @@ export class CoursesComponent implements OnInit {
     path: "Courses"
    }
 
-   courses = ["", "", "", "", ""]
+   courses: any;
 
-  constructor( private title: Title) {
+   categories: any;
+   category = 'all';
+   keywords: string;
+
+  constructor( private title: Title, private route: ActivatedRoute, public restApi: RestApiService, private time: TimeService) {
     this.title.setTitle("Mystart | Courses")
    }
   ngOnInit() {
+    this.restApi.get('categories').subscribe((data: {}) => {
+      this.categories = data;
+    });
+    this.restApi.get('courses').subscribe((data: {}) => {
+      this.courses = data;
+      let rateSum: any;
+      for (let course of this.courses) {
+        rateSum = 0;
+        if (course.price == 0) course.price = 'Free';
+        else course.price = course.price + ' TND';
+        course.createdAt = new Date(course.createdAt);
+        course.description = course.description.substring(0,130) + '...';
+        for (let review of course.reviews) {
+          rateSum += parseInt(review.rate);
+        }
+        course.rate = new Number(rateSum / course.reviews.length);
+        course.rate = parseFloat(course.rate.toFixed(1));
+      }
+    });
+
   }
 
 }
