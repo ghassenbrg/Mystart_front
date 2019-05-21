029d12e807b967c11685f8739c177315239b0d7b
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-05-21 18:51:53 +0100

    course-single pahe front-end

--- src/app/pages/course-single/course-single.component.ts
+++ src/app/pages/course-single/course-single.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseSingleComponent implements OnInit {
 
+  params = {
+    title: "This is My first title",
+    description: "",
+    path: "Courses"
+  }
+
+  lessons = ['Introduction Lesson', 'Basics of HTML', 'Getting Know about HTML', 'Tags and Attributes', 
+             'Basics of CSS', 'Getting Familiar with CSS', 'Introduction to Bootstrap', 'Responsive Design'];
+
+  attachments = ['objectives.pdf', 'summary.ppt', 'book.docx'];
+
   constructor() { }
 
   ngOnInit() {
