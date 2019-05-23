8fef89b3ee2f604f03f75f3c92052679274bdfa0
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-05-23 03:01:31 +0100

    get single lesson from api

--- src/app/pages/lessons/lesson-single/lesson-single.component.ts
+++ src/app/pages/lessons/lesson-single/lesson-single.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LessonsComponent } from '../lessons.component';
+import { RestApiService } from 'src/app/core/rest-api.service';
 
 @Component({
   selector: 'app-lesson-single',
@@ -10,14 +11,20 @@ import { LessonsComponent } from '../lessons.component';
 export class LessonSingleComponent implements OnInit {
 
   nbr: any;
-  lesson: {};
-  constructor(private route: ActivatedRoute, private parent: LessonsComponent) { }
+  lesson: any;
+  constructor(private route: ActivatedRoute, private parent: LessonsComponent, private restApi: RestApiService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.nbr= params.get('nbr');
       this.parent.nbr = this.nbr;
+      this.restApi.get('course/'+this.parent.id+'/lesson/'+this.nbr).subscribe((data: {}) => {
+        this.lesson = data;
+      });
     });
   }
 
+  alert(){
+    window.alert(JSON.stringify(this.lesson));
+  }
 }
