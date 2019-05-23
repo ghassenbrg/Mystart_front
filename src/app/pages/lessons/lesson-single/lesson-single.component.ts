14e6f243b5d8b86b7fb3496e451d7bcf8700583b
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-05-23 04:06:05 +0100

    try to bind youtube iframe

--- src/app/pages/lessons/lesson-single/lesson-single.component.ts
+++ src/app/pages/lessons/lesson-single/lesson-single.component.ts
@@ -12,6 +12,8 @@ export class LessonSingleComponent implements OnInit {
 
   nbr: any;
   lesson: any;
+  test = 'https://www.youtube.com/embed/fBNz5xF-Kx4';
+
   constructor(private route: ActivatedRoute, private parent: LessonsComponent, private restApi: RestApiService) { }
 
   ngOnInit() {
@@ -20,11 +22,19 @@ export class LessonSingleComponent implements OnInit {
       this.parent.nbr = this.nbr;
       this.restApi.get('course/'+this.parent.id+'/lesson/'+this.nbr).subscribe((data: {}) => {
         this.lesson = data;
+        this.lesson.videoUrl = '<iframe *ngIf="lesson" width="100%" height="450" src="https://www.youtube.com/embed/'+this.youtube_parser(this.lesson.videoUrl)+'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        //this.lesson.videoUrl = "<h1> Hello <strong>world</strong> !</h1>"
       });
     });
   }
 
+  youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+}
+
   alert(){
-    window.alert(JSON.stringify(this.lesson));
+    window.alert(this.youtube_parser(this.lesson.videoUrl));
   }
 }
