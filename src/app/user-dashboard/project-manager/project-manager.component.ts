a7ca00438c510305426e0c97398f603427e22c77
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-06-04 02:26:14 +0100

    add new project html elements

--- src/app/user-dashboard/project-manager/project-manager.component.ts
+++ src/app/user-dashboard/project-manager/project-manager.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { RestApiService } from 'src/app/core/rest-api.service';
 import { UserDashLayoutComponent } from '../user-dash-layout/user-dash-layout.component';
+import { NzModalRef, NzModalService, NzNotificationService } from 'ng-zorro-antd';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-project-manager',
@@ -13,11 +15,22 @@ export class ProjectManagerComponent implements OnInit {
   listOfData: any[] = [];
   projects: any;
   loading = false;
+  categories: any;
+  //var for mdoal
+  tplModal: NzModalRef;
+  tplModalButtonLoading = false;
+  //editor
+  public Editor = ClassicEditor;
+  content = '<p>Hello world !</p>';
 
-  constructor(private restApi: RestApiService, private parent: UserDashLayoutComponent) { }
+  constructor(private restApi: RestApiService, private parent: UserDashLayoutComponent,
+     private modalService: NzModalService, private notification: NzNotificationService) { }
 
   ngOnInit(): void {
     this.loadData();
+    this.restApi.get('categories').subscribe((data: {}) => {
+      this.categories = data;
+    });
   }
   
   loadData(){
@@ -66,8 +79,42 @@ export class ProjectManagerComponent implements OnInit {
     this.i++;
   }
 
-  deleteRow(id: string): void {
-    this.listOfData = this.listOfData.filter(d => d.id !== id);
+  deleteRow(id: string,i): void {
+    let title = this.listOfData[i].title;
+    this.restApi.delete('project/'+id).subscribe((data: {}) => {
+      this.listOfData = this.listOfData.filter(d => d.id !== id);
+      this.notification.create(
+        'error',
+        'Project deleted',
+        'The project with title: '+title+' is successfully deleted.'
+      );
+    });  
+  }
+
+  // popup methods
+  createTplModal(tplTitle: TemplateRef<{}>, tplContent: TemplateRef<{}>, tplFooter: TemplateRef<{}>): void {
+    this.tplModal = this.modalService.create({
+      nzTitle: tplTitle,
+      nzContent: tplContent,
+      nzFooter: tplFooter,
+      nzMaskClosable: true,
+      nzClosable: true,
+      nzWidth: "50%",
+      nzOnOk: () => console.log('Click ok')
+    });
+  }
+
+  destroyTplModal(): void {
+    this.tplModalButtonLoading = true;
+    setTimeout(() => {
+      this.tplModalButtonLoading = false;
+      this.tplModal.destroy();
+      this.notification.create(
+        'success',
+        'Add new project',
+        'The project is successfully added.'
+      );
+    }, 1000);
   }
 
 }
