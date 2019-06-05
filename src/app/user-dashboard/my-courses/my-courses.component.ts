cbe10e5acfbf822ad37e4fd9812374521e5d62a2
Ghassen Bargougui <ghassen.brg16@gmail.com>
2019-06-06 00:39:29 +0100

    my courses elements

--- src/app/user-dashboard/my-courses/my-courses.component.ts
+++ src/app/user-dashboard/my-courses/my-courses.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from 'src/app/core/rest-api.service';
+import { NzNotificationService } from 'ng-zorro-antd';
+import { UserDashLayoutComponent } from '../user-dash-layout/user-dash-layout.component';
 
 @Component({
   selector: 'app-my-courses',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyCoursesComponent implements OnInit {
 
-  constructor() { }
+  courses: any;
+
+  constructor( private restApi: RestApiService, private parent: UserDashLayoutComponent, private notification: NzNotificationService) { }
 
   ngOnInit() {
+    this.restApi.get('user/'+this.parent.loggedUser._id+'/courses').subscribe((data: {}) => {
+      this.courses = data;
+      this.notification.create(
+        'success',
+        'Authorized list was updated',
+        JSON.stringify(this.courses[0].title)
+      );
+    });
+  }
+
+  loadData() {
+
   }
 
 }
